eb9fb4b7c71f75d847123ba8ece6360bf77b3f33
UX100 TRAIN-00 <i016416@gmail.com>
2025-12-10 05:56:48 +0000

    move ZDEMO2 to another package

--- src/zdemo2_params.wapa.component.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"],function(e){"use strict";return e.extend("demo2params.Component",{metadata:{manifest:"json"}})});                                                                                             
-//# sourceMappingURL=Component.js.map                                                                                                                                                                                                                          
